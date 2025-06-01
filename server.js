7dfd9125b4c1aa0fdf35df8320ae29c158398c57
ARK2K <ark11.01.2000@gmail.com>
2025-06-01 18:17:15 +0530

    feat: Add error handling middleware to manage server errors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -45,6 +46,8 @@ app.use('/api', healthRoutes);
 app.get('/', (req, res) => {
   res.send('Recipe Book API is running!');
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
