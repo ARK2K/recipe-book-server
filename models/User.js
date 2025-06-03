b65b5a490276d3258b81e92817da7f98731b3936
ARK2K <ark11.01.2000@gmail.com>
2025-06-03 22:54:50 +0530

    refactor: Improve code readability in authMiddleware and User model by simplifying function calls and removing unnecessary comments

--- models/User.js
+++ models/User.js
@@ -8,17 +8,16 @@ const UserSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now }
 });
 
-// Hash password before saving
-UserSchema.pre('save', async function(next) {
+UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
-    next();
+    return next();
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
-// Method to compare password
-UserSchema.methods.matchPassword = async function(enteredPassword) {
+UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
