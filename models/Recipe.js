24eadb6fb90a16d705af7032731607c6acfea626
ARK2K <ark11.01.2000@gmail.com>
2025-07-11 10:53:49 +0530

    refactor: Rename image field to imageUrl in Recipe schema for consistency

--- models/Recipe.js
+++ models/Recipe.js
@@ -6,7 +6,7 @@ const RecipeSchema = new mongoose.Schema({
   description: { type: String, required: true },
   ingredients: [{ type: String, required: true }],
   instructions: { type: String, required: true },
-  image: { type: String, default: '' },
+  imageUrl: { type: String, default: '' },
   category: { type: String },
   tags: [{ type: String }],
   favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
